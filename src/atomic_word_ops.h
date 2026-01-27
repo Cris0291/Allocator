c279f6d52fcd7513dd509f0a9ba67c39357a01a0
Cristian Blanco <cris0291@gmail.com>
2026-01-26 21:23:48 -0600

    add bitmasks creation

--- src/atomic_word_ops.h
+++ src/atomic_word_ops.h
@@ -8,7 +8,7 @@ static inline std::uint64_t atomic_word_fetch_and(std::uint64_t *p,
   return __atomic_fetch_and(p, mask, __ATOMIC_ACQ_REL);
 }
 
-static inline std::uint64_t atomic_word_fetcg_or(std::uint64_t *p,
+static inline std::uint64_t atomic_word_fetch_or(std::uint64_t *p,
                                                  std::uint64_t mask) {
   return __atomic_fetch_or(p, mask, __ATOMIC_ACQ_REL);
 }
