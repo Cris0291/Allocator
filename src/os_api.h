3af858419418e1c4f92250c54abc61135b1bcb1e
Cristian Blanco <cris0291@gmail.com>
2026-02-10 21:10:40 -0600

    start implementing os specifics for interface

--- src/os_api.h
+++ src/os_api.h
@@ -32,5 +32,14 @@ bool os_supports_huge_pages();
 
 OsResult reserve_address_space(std::size_t size, std::size_t alignment,
                                MemSpan &out);
+OsResult commit_memory(void* addr, std::size_t length);
+OsResult decommit_memory(void* addr, std::size_t length);
+OsResult release_addresss_space(void* addr, std::size_t length);
+OsResult reserve_with_hugh_pages(std::size_t size, std::size_t alignment, MemSpan &out);
+OsResult set_protection(void* addr, std::size_t lenght, int prop_flags);
+OsResult advice(void* addr, std::size_t length, Advice advice);
+
+std::size_t query_commited_memory(void* addr, std::size_t size);
+
 
 } // namespace os_api
