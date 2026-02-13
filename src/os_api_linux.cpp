0658fc1bc743ea2f81de9114d73495bf12588155
Cristian Blanco <cris0291@gmail.com>
2026-02-12 21:13:10 -0600

    struggling as always

--- src/os_api_linux.cpp
+++ src/os_api_linux.cpp
@@ -1,8 +1,13 @@
 #include "os_api.h"
+#include <cstddef>
+#include <cstdint>
 #include <sys/mman.h>
 
+std::uintptr_t os_api::align_up(std::uintptr_t addr, std::size_t size) {
+  return (addr + (size - 1)) & ~(size - 1);
+}
 os_api::OsResult os_api::reserve_address_space(std::size_t size,
                                                std::size_t alignment,
                                                MemSpan &out) {
-  mmap(void *addr, size_t len, int prot, int flags, int fd, __off_t offset)
+  g mmap(void *addr, size_t len, int prot, int flags, int fd, __off_t offset)
 }
