e5be9ae2cea66bf5722b86f3a312cf337335b1d9
Cristian Blanco <cris0291@gmail.com>
2026-03-31 20:48:39 -0600

    keep working on the red black tree

--- src/red_black_tree.cpp
+++ src/red_black_tree.cpp
@@ -1,4 +1,4 @@
-enum class RBColor { Red, Black };
+enum class RBColor : unsigned long { Red, Black };
 
 // this is based on the kernel version of a rb tree
 // see what will happen on 32 bit machine
@@ -10,13 +10,49 @@ struct RBNode {
 };
 
 struct RBRoot {
-  RBNode *root = nullptr;
+  RBNode *rb_root = nullptr;
 };
 
 inline RBNode *get_rb_parent(const RBNode *rb_node) {
   return reinterpret_cast<RBNode *>(rb_node->rb_parent_color & ~3UL);
 };
 
-inline unsigned long get_rb_color(RBNode *rb_node) {
+inline unsigned long get_rb_color(const RBNode *rb_node) {
   return rb_node->rb_parent_color & 1;
 };
+
+inline bool is_rb_red(const RBNode *rb_node) {
+  return !(rb_node->rb_parent_color & 1);
+};
+
+inline bool is_rb_black(const RBNode *rb_node) {
+  return !!(rb_node->rb_parent_color & 1);
+};
+
+inline void rb_set_parent_color(RBNode *rb_node, RBNode *parent, int color) {
+  rb_node->rb_parent_color = reinterpret_cast<unsigned long>(parent) + color;
+};
+
+inline void rb_set_parent(RBNode *rb_node, RBNode *parent) {
+  rb_node->rb_parent_color =
+      reinterpret_cast<unsigned long>(parent) + get_rb_color(rb_node);
+};
+
+inline void rb_set_black(RBNode *rb_node) { rb_node->rb_parent_color |= 1UL; };
+
+inline RBNode *get_rb_red_parent(RBNode *rb_node) {
+  return reinterpret_cast<RBNode *>(rb_node->rb_parent_color);
+};
+
+inline void rb_change_child(RBNode *old_node, RBNode *new_node, RBNode *parent,
+                            RBRoot *root) {
+  if (parent) {
+    if (parent->left == old_node) {
+      parent->left = new_node;
+    } else {
+      parent->right = new_node;
+    }
+  } else {
+    root->rb_root = new_node;
+  }
+}
